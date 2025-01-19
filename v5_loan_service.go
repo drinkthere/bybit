3b60014a224ffef7eb76d69948e924ee6e7ba4ec
dictwang <dictwang@163.com>
2025-01-19 20:21:13 +0800

    add collateral data api

--- v5_loan_service.go
+++ v5_loan_service.go
@@ -5,6 +5,55 @@ import "github.com/google/go-querystring/query"
 // V5LoanServiceI :
 type V5LoanServiceI interface {
 	GetLoanableData(param V5GetLoanableDataParam) (*V5GetLoanableDataResponse, error)
+	GetCollateralData(param V5GetCollateralDataParam) (V5GetCollateralDataResponse, error)
+}
+
+// V5GetCollateralDataParam :
+type V5GetCollateralDataParam struct {
+	VipLevel string `json:"vipLevel,omitempty"`
+	Currency string `json:"currency,omitempty"`
+}
+
+// V5GetCollateralDataResponse :
+type V5GetCollateralDataResponse struct {
+	CommonV5Response `json:",inline"`
+	Result           V5GetCollateralDataResult `json:"result"`
+}
+
+// V5GetCollateralDataResult :
+type V5GetCollateralDataResult struct {
+	VipCoinList []V5GetCollateralVipCoinListItem `json:"vipCoinList"`
+}
+
+// V5GetCollateralVipCoinListItem :
+type V5GetCollateralVipCoinListItem struct {
+	List     []V5GetCollateralVipCoinListInnerItem `json:"list"`
+	VipLevel string                                `json:"vipLevel"`
+}
+
+// V5GetCollateralVipCoinListInnerItem :
+type V5GetCollateralVipCoinListInnerItem struct {
+	Currency           string `json:"currency"`
+	CollateralAccuracy int    `json:"collateralAccuracy"`
+	InitialLTV         string `json:"initialLTV"`
+	MarginCallLTV      string `json:"marginCallLTV"`
+	MaxLimit           string `json:"maxLimit"`
+	LiquidationLTV     string `json:"liquidationLTV"`
+}
+
+func (s *V5LoanService) GetCollateralData(param V5GetCollateralDataParam) (*V5GetCollateralDataResponse, error) {
+	var res V5GetCollateralDataResponse
+
+	queryString, err := query.Values(param)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.client.getPublicly("/v5/crypto-loan/collateral-data", queryString, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 // V5GetLoanableDataParam :
